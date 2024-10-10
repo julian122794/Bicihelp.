6f85917bb39541fed815992ecce37358c494bcbb
julian122794 <104115623+julian122794@users.noreply.github.com>
2024-10-09 19:12:46 -0500

    Update Servicios.php

--- Bicihelp/VIEW/Servicios.php
+++ Bicihelp/VIEW/Servicios.php
@@ -1,58 +1,65 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BiciHelp - Inicio</title>
     <link rel="stylesheet" href="../CSS/Inicio.css">
 </head>
+
 <body>
-    
+
     <header>
-        <div class = "logo">
-            <img src="C:\xampp\htdocs\Bicihelp\IMG\logo.jpg" width="150" height="200" alt="25">
-        <h1>Nuestros servicios</h1>
+        <div class="logo">
+            <img src="../IMG/logo.jpg" width="150" height="200" alt="25">
+            <h1>Nuestros servicios</h1>
         </div>
     </header>
     <nav>
         <ul>
-                 <li><a href="../VIEW/Inicio.php">Inicio</a></li>
+            <li><a href="../VIEW/Inicio.php">Inicio</a></li>
             <li><a href="../VIEW/Nosotros.php">Quienes somos</a></li>
-            <li><a href="#">Contactenos</a></li>
+            <li><a href="../VIEW/contactenos.php">Contactenos</a></li>
             <li><a href="../VIEW/Servicios.php">servicios</a></li>
             <li><a href="../VIEW/Registro_contratista.php">Registrarse</a></li>
             <li><a href="../VIEW/Inicio_sesion.php">Inicio de sesion</a></li>
-            <li><a href="#">Chat</a></li>
+            <li><a href="../VIEW/chat.php">Chat</a></li>
         </ul>
     </nav>
     <main>
-        
+
         <section>
             <h2>Servicios electricos</h2>
             <p>Nuestro servicio de reparación y mantenimiento de bicicletas eléctricas se especializa en diagnosticar y solucionar problemas relacionados con los sistemas eléctricos de tu bici. Ofrecemos la revisión y reparación de baterías, motores, cables, controladores y todo el sistema eléctrico para asegurar un funcionamiento óptimo y seguro. Además, realizamos mantenimiento preventivo, cambios de componentes y ajustes para maximizar la vida útil de tu bicicleta eléctrica, garantizando que siempre esté lista para rodar con el mejor rendimiento.</p>
-            <div class = "reparación">
-                <img src="IMG\ReparacionselectroniquesVAIC.jpg" width="650" height="400" alt="45">
-            
-       
+            <div class="reparación">
+                <img src="../IMG/ReparacionselectroniquesVAIC.jpg" width="650" height="400" alt="45">
+
+
         </section>
         <section>
             <h2>Servicio técnico</h2>
             <p>Ofrecemos un servicio de préstamo técnico de bicicletas diseñado para asegurar que tu bicicleta esté siempre en perfectas condiciones, sin necesidad de que adquieras herramientas especializadas o conocimientos técnicos. Nuestro objetivo es brindarte un mantenimiento y reparación de calidad, facilitando la experiencia de ciclismo y prolongando la vida útil de tu bicicleta.
-                <div class = "reparación">
-                    <img src="IMG\tecnico.jpg" width="650" height="400" alt="45">
-            </p>
+            <div class="reparación">
+                <img src="../IMG/tecnico.jpg" width="650" height="400" alt="45">
+                </p>
         </section>
         <section>
             <h2>Repuestos</h2>
             <p>En BiciHlep, nos especializamos en ofrecer una amplia gama de repuestos para bicicletas, asegurando que puedas mantener y reparar tu bicicleta con facilidad y eficacia. Contamos con un inventario completo de piezas y accesorios de alta calidad, adaptados a diferentes tipos de bicicletas y necesidades específicas.
-                <div class = "reparación">
-                    <img src="IMG\repuestos bicicleta.jpg" width="650" height="400" alt="45">
-            </p>
+            <div class="reparación">
+                <img src="../IMG/repuestos bicicleta.jpg" width="650" height="400" alt="45">
+                </p>
         </section>
     </main>
     <br></br><br></br><br></br><br></br><br></br><br></br>
     <?php
 
+    include("footer.php")
+    ?>
+</body>
+
+</html>
 include("footer.php")
 ?>
 </body>
