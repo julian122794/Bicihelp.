26bd6e56902d76434d749012795bd65a724e5672
julian122794 <104115623+julian122794@users.noreply.github.com>
2024-10-09 19:10:34 -0500

    Update Nosotros.php

--- Bicihelp/VIEW/Nosotros.php
+++ Bicihelp/VIEW/Nosotros.php
@@ -4,16 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contactenos</title>
-    <link rel="stylesheet" href="../CSS/contactenos.css">
+    <title>NOSOTROS</title>
+    <link rel="stylesheet" href="../CSS/Inicio.css">
 </head>
 
 <body>
 
     <header>
         <div class="logo">
-            <img src="../IMG/logo.jpg" width="100" height="100" alt="25">
-            <h1>Contactenos</h1>
+            <img src="../IMG/logo.jpg" width="150" height="200" alt="25">
+            <h1>NOSOTROS</h1>
         </div>
     </header>
     <nav>
@@ -25,35 +25,25 @@
             <li><a href="../VIEW/Registro_contratista.php">Registrarse</a></li>
             <li><a href="../VIEW/Inicio_sesion.php">Inicio de sesión</a></li>
             <li><a href="../VIEW/chat.php">Chat</a></li>
+
         </ul>
     </nav>
-    <div class="container-form">
-        <div class="info-form">
-            <h2>coctactenos</h2>
-            <p>¡Bienvenido a nuestro Taller de Bicicletas! Si tienes alguna pregunta, necesitas más información sobre nuestros</br>servicios o quieres agendar una cita,
-                estamos aquí para ayudarte. Nos apasiona mantener tu bicicleta en perfectas condiciones,</br> ya sea para reparaciones, mantenimiento o personalización.
-                Puedes contactarnos a través del formulario a continuación,</br> llamarnos por teléfono o visitarnos directamente en nuestro taller.
-                ¡Estamos listos para ofrecerte un servicio rápido</br> y de calidad, para que sigas pedaleando sin preocupaciones!</p></br></br></br>
+    <main>
 
-            <i class='bx bxs-phone'>3165338316</i>
-            <i class='bx bxs-envelope'>bicihelp@outlook.com</i>
-            <i class='bx bxs-map'>ciudad,pais</i>
-        </div>
-        <form action="#" autocomplete="off">
-            <p class="subtitle">Nombre completo</p>
-            <input type="text" name="nombre"
-                placeholder="ingrese   su nombre" class="campo"></br></br>
-            <p class="subtitle">Correo electronico</p>
-            <input type="email" name="email"
-                placeholder="ingrese su correo" class="campo"></br></br>
-            <p class="subtitle">Quejas o sugerencias</p>
-            <textarea name="mensaje"
-                placeholder="ingrese su mensaje">
-        </textarea>
-            <input type="submit" name="enviar" value="enviar mensaje" class="btn-enviar">
+        <section>
+            <h2>BiciHelp</h2>
+            <p>¿Quiénes somos?
 
-        </form>
-    </div>
+                Somos un equipo apasionado por la movilidad sostenible y la tecnología, dedicados a facilitar la vida de los ciclistas urbanos. Nuestro objetivo es conectar de manera instantánea a los usuarios de bicicletas con técnicos especializados, brindando soluciones rápidas y eficientes para los contratiempos que puedan surgir en sus desplazamientos diarios.
+
+                Entendemos que mientras los ciclistas enfrentan una variedad de desafíos recorren la ciudad, desde pinchazos y problemas mecánicos hasta situaciones de extravío. Es por ello que nos hemos propuesto identificar exhaustivamente cada uno de estos escenarios, para ofrecer un servicio ágil y preciso que cubra todas sus necesidades.
+
+                Nuestra aplicación móvil está diseñada para ser el aliado perfecto en el camino, garantizando que cada ciclista pueda llegar a su destino sin interrupciones. Con un enfoque centrado en la eficiencia y la confiabilidad, buscamos transformar la experiencia del ciclismo urbano, haciendo que cada recorrido sea seguro y libre de preocupaciones.
+
+                Creemos en el poder de la comunidad y en la importancia de mantener a los ciclistas en movimiento, por lo que trabajamos incansablemente para ofrecer un servicio que no solo resuelva problemas, sino que también promueva un estilo de vida activo y saludable. ¡Únete a nosotros y descubre una nueva forma de rodar por la ciudad!</p>
+        </section>
+
+    </main>
     <?php
 
     include("footer.php")
