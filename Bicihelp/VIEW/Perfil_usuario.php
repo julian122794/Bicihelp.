953002b9b0cc64fb98b8e26c8c220decbcc4cce4
Joaquin Rodriguez <jalexander.rodriguez@gmail.com>
2024-10-10 22:37:43 -0500

    Update Perfil_usuario.php
    MODIFICAR LA RUTA DE C.S.S A CSS

--- Bicihelp/VIEW/Perfil_usuario.php
+++ Bicihelp/VIEW/Perfil_usuario.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Profile</title>
-    <link rel="stylesheet" href="../C.S.S/perfil.css">
+    <link rel="stylesheet" href="../CSS/perfil.css">
 </head>
 <header>
     <div class = "logo">
