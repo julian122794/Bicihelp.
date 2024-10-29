933011666eaa4d15a5e0bd52671b6af6da472b07
galindohernan <h.d.g.c789@gmail.com>
2024-10-28 21:19:28 -0500

    Update Nosotros.php

--- Bicihelp/VIEW/Nosotros.php
+++ Bicihelp/VIEW/Nosotros.php
@@ -1,53 +1,46 @@
-<!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NOSOTROS</title>
-    <link rel="stylesheet" href="../CSS/Inicio.css">
+    <title>Inicio de Sesión</title>
+    <link rel="stylesheet" href="../CSS/Styles.css">
 </head>
 
-<body>
-
+<body>   
     <header>
-        <div class="logo">
-            <img src="../IMG/logo.jpg" width="150" height="200" alt="25">
-            <h1>NOSOTROS</h1>
+        <div class = "logo">
+            <img src="../IMG/logo.jpg" width="150" height="150" alt="25">
+        <h1 class="title">Bienvenido a BiciHelp</h1>
         </div>
-    </header>
-    <nav>
-        <ul>
-            <li><a href="../VIEW/Inicio.php">Inicio</a></li>
-            <li><a href="../VIEW/Nosotros.php">Quienes somos</a></li>
-            <li><a href="../VIEW/contactenos.php">Contactenos</a></li>
-            <li><a href="../VIEW/Servicios.php">servicios</a></li>
-            <li><a href="../VIEW/Registro_contratista.php">Registrarse</a></li>
-            <li><a href="../VIEW/Inicio_sesion.php">Inicio de sesión</a></li>
-            <li><a href="../VIEW/chat.php">Chat</a></li>
-
-        </ul>
-    </nav>
-    <main>
-
-        <section>
-            <h2>BiciHelp</h2>
-            <p>¿Quiénes somos?
-
-                Somos un equipo apasionado por la movilidad sostenible y la tecnología, dedicados a facilitar la vida de los ciclistas urbanos. Nuestro objetivo es conectar de manera instantánea a los usuarios de bicicletas con técnicos especializados, brindando soluciones rápidas y eficientes para los contratiempos que puedan surgir en sus desplazamientos diarios.
-
-                Entendemos que mientras los ciclistas enfrentan una variedad de desafíos recorren la ciudad, desde pinchazos y problemas mecánicos hasta situaciones de extravío. Es por ello que nos hemos propuesto identificar exhaustivamente cada uno de estos escenarios, para ofrecer un servicio ágil y preciso que cubra todas sus necesidades.
-
-                Nuestra aplicación móvil está diseñada para ser el aliado perfecto en el camino, garantizando que cada ciclista pueda llegar a su destino sin interrupciones. Con un enfoque centrado en la eficiencia y la confiabilidad, buscamos transformar la experiencia del ciclismo urbano, haciendo que cada recorrido sea seguro y libre de preocupaciones.
-
-                Creemos en el poder de la comunidad y en la importancia de mantener a los ciclistas en movimiento, por lo que trabajamos incansablemente para ofrecer un servicio que no solo resuelva problemas, sino que también promueva un estilo de vida activo y saludable. ¡Únete a nosotros y descubre una nueva forma de rodar por la ciudad!</p>
-        </section>
-
-    </main>
+       
+    </header> 
+    <div class="login-container">
+        <h2>Inicio de Sesión</h2>
+      <form action="../CONTROLER/ingreso.php" method="post">
+            <div class="input-group">
+                <label for="username">Usuario</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="input-group">
+                <label for="password">Contraseña</label>
+                <input type="password" id="password" name="password" required>
+                <span class="eye-icon">&#128065;</span>
+            </div>
+            <a href="">¿olvidaste tu contraseña?</a>  </br></br>
+            <button type="submit">Iniciar Sesión</button>
+            <br></br>
+            <button type="submit">Iniciar con GOOGLE</button>
+            <br></br>
+            <button type="submit">Iniciar con IOS </button></br></br>
+            
+            <div>
+            <button type="submit">volver</button>
+            </div>
+        </form>
+    </div>
     <?php
 
-    include("footer.php")
-    ?>
+include("footer.php")
+?>
 </body>
-
 </html>
